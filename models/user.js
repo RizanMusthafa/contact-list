4b62c5d13a8827b5ef4069db888407bc3f7aa2b0
Rizan Mohomed <musthafarizan@gmail.com>
2018-10-04 10:19:40 +0530

    created user model

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const userSchema = new mongoose.Schema({
+  fName: {
+    type: String,
+    maxlength: 150,
+    minlength: 2,
+    trim: true
+  },
+  sName: {
+    type: String,
+    maxlength: 150,
+    minlength: 2,
+    trim: true
+  },
+  email: {
+    type: String,
+    maxlength: 150,
+    minlength: 2,
+    trim: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const User = mongoose.model('user', userSchema);
+
+function validateUser(user) {
+  return Joi.validate(user, {
+    fName: Joi.string()
+      .required()
+      .min(2)
+      .max(150),
+    sName: Joi.string()
+      .required()
+      .min(2)
+      .max(150),
+    email: Joi.string()
+      .required()
+      .min(2)
+      .max(150)
+      .trim()
+      .email()
+  });
+}
+
+module.exports = { User, validateUser };
